d15f7845906213b26e94aafeb43122e0e0663031
GTZhechev18 <58167676+GTZhechev18@users.noreply.github.com>
2021-01-03 19:29:40 +0200

    Add files via upload

--- project Scalefocus/project Scalefocus/project Scalefocus.cpp
+++ project Scalefocus/project Scalefocus/project Scalefocus.cpp
@@ -2,8 +2,15 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <conio.h>
+#include<time.h>
 
 using namespace std;
+
+vector<int> history;
+
+
 
 void Digits(int n, int digs[4])
 {
@@ -15,11 +22,47 @@ void Digits(int n, int digs[4])
         br--;
     }
 }
+int GenerateNumber()
+{
+    srand(time(NULL));
+    int n;
+    bool correct = true;
+    do {
+        n = rand() % 9000 + 1000;
+        correct = true;
+        int digs[4];
+        Digits(n, digs);
+
+        for (int i = 0; i < 4; i++)
+            if (digs[i] < 0 or digs[i]>7) {
+                correct = false;
+                // cout << "The digits have to be between 0 and 7!\n";
+                break;
+            }
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = i + 1; j < 4; j++)
+                if (digs[i] == digs[j]) {
+                    correct = false;
+                    break;
+                }
+            if (correct == false) break;
+        }
+    } while (correct == false);
+    return n;
+}
 int ReadNumber()
 {
     int n;
     cout << "Enter number:";
     cin >> n;
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Enter number:";
+        cin >> n;
+        cout << endl;
+    }
     while (n < 1000 or n>9999)
     {
         cout << "The number has to be 4-digit!\n";
@@ -64,6 +107,8 @@ int EnterNumber(bool diff)
 
     return n;
 }
+
+
 void CheckGuess(int Coords, int Guess, int &Reds, int&Greens)
 {
     int CoordsDigs[4], GuessDigs[4];
@@ -78,9 +123,19 @@ void CheckGuess(int Coords, int Guess, int &Reds, int&Greens)
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             if (CoordsDigs[i] == GuessDigs[j] and i != j) Reds++;
+  //  call GuessResult(Reds,Greens) ;
   //  cout << "Red=" << Reds << "\nGreen=" << Greens;
 }
+void ShowHistory(int Coords)
+{
+    for (unsigned int i = 0; i < history.size(); i++)
+    {
+        int R, G;
+        CheckGuess(Coords, history[i], R, G);
+        cout << "Guess " << i + 1 <<" : "<<history[i]<< " Reds=" << R << " Greens=" << G << endl;
+    }
 
+}
 void Level1()
 {
     int number1;
@@ -91,19 +146,58 @@ void Level1()
     bool Guessed = false;
     int Reds, Greens;
     while (GuessNum < 13 and Guessed == false)
-    {
+    {   
+        system("CLS");
         GuessNum++;
         int number2;
         cout << "Guess number "<< GuessNum<<"!\nPlayer 2 enter your guess:";
         number2 = EnterNumber(true);
+        history.push_back(number2);
         CheckGuess(number1, number2, Reds, Greens);
-        cout << "Red=" << Reds << "\nGreen=" << Greens<<endl;
+        ShowHistory(number1);
+      //  cout << "Red=" << Reds << "\nGreen=" << Greens<<endl;
+      //  GuessResult(Reds, Greens);
+        if (Greens < 4) {
+            cout << "Ready for the next guess? Press any key to continue! ";
+            _getch();
+        }
         if (Greens == 4) {
                          cout << "Congratulations! You have broken the code!";
                          Guessed = true;
                         }
     }
-    if (Guessed == false) cout << "Sorry! You failed trying to break the code!";
+    if (Guessed == false) cout << "Sorry! You failed trying to break the code! The code is " << number1 << endl;
+}
+
+void Level2()
+{
+    int number1 = GenerateNumber();
+    int GuessNum = 0;
+    bool Guessed = false;
+    int Reds, Greens;
+    while (GuessNum < 13 and Guessed == false)
+    {
+            system("CLS");
+            GuessNum++;
+            int number2;
+            cout << "Guess number " << GuessNum << "!\nPlayer, enter your guess:";
+            number2 = EnterNumber(true);
+            history.push_back(number2);
+            CheckGuess(number1, number2, Reds, Greens);
+            ShowHistory(number1);
+            //  cout << "Red=" << Reds << "\nGreen=" << Greens<<endl;
+            //  GuessResult(Reds, Greens);
+            if (Greens < 4) {
+                cout << "Ready for the next guess? Press any key to continue! ";
+                _getch();
+            }
+            if (Greens == 4) {
+                cout << "Congratulations! You have broken the code!";
+                Guessed = true;
+            }
+        }
+        if (Guessed == false) cout << "Sorry! You failed trying to break the code!" << number1 << endl;
+
 }
 
 void MainMenu()
@@ -119,11 +213,12 @@ void MainMenu()
         cin >> level;
     }
     if (level == 1) Level1();
-   // else Level2();
+      else Level2();
 }
 
 int main()
 {
+  //  cout << GenerateNumber();
     MainMenu();
    /* int a = EnterNumber(true);
     cout << a;
